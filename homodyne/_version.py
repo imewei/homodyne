64905581747abbdaefc3cc2e02e6841448ee6cf9
Wei Chen <wchen@anl.gov>
2025-10-28 17:23:37 -0500

    chore: bump version to 2.0.0.post24

--- homodyne/_version.py
+++ homodyne/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '2.0.0.post20+dirty'
-__version_tuple__ = version_tuple = (2, 0, 0, 'post20', 'dirty')
+__version__ = version = '2.0.0.post24+dirty'
+__version_tuple__ = version_tuple = (2, 0, 0, 'post24', 'dirty')
 
-__commit_id__ = commit_id = 'g30b091222'
+__commit_id__ = commit_id = 'g1f82eddff'
