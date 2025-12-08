7a86b5c0cd0316c8efc36377a8000c95e4f08977
Wei Chen <wchen@anl.gov>
2025-12-08 10:01:09 -0600

    refactor(optimization): remove unused initialization module
    Delete homodyne/optimization/initialization/ module which contained
    PerPhiInitializer class that was never used in the codebase. Initial
    values are now handled directly in CMC priors.py.

--- homodyne/optimization/initialization/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Initialization utilities for optimization pipelines."""
-
-from .per_phi_initializer import PerPhiInitConfig, build_per_phi_initial_values
-
-__all__ = ["PerPhiInitConfig", "build_per_phi_initial_values"]
